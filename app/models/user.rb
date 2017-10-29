1e4cb04589b4a762ccab52b1af213311e20e3810
Felipe <fbarrios.ing.informatico@gmail.com>
2017-10-28 22:22:47 -0300

    ADD: method authentication_by_image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  def authentication_by_image external_image
+    percent = image_difference_percent(external_image)
+    return true if percent <= 0.10
+  end
+
+  def image_difference_percent(external_image)
+    longer = [image.size, external_image.size].max
+    same   = image.each_char.zip(external_image.each_char).select { |a,b| a == b }.size
+    (longer - same) / image.size.to_f
+  end
 end
