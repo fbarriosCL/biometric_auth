44afac68283b7d5a0ad241712aab20b364ba8527
Felipe <fbarrios.ing.informatico@gmail.com>
2017-10-28 22:59:38 -0300

    HOTFIX: model user and route url

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   def authentication_by_image external_image
     percent = image_difference_percent(external_image)
     return true if percent <= 0.10
+    false
   end
 
   def image_difference_percent(external_image)
@@ -10,4 +11,5 @@ class User < ApplicationRecord
     same   = image.each_char.zip(external_image.each_char).select { |a,b| a == b }.size
     (longer - same) / image.size.to_f
   end
+
 end
