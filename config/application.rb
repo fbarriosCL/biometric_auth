7f125855838df180da7d7611ea128d1e269af9f0
Felipe <fbarrios.ing.informatico@gmail.com>
2017-10-31 19:41:11 -0300

    ADD: config sidekiq

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 module BiometricAuth
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.active_job.queue_adapter = :sidekiq
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web, at: "/sidekiq"
   root to: 'users#index'
   resources :users
   post 'rest/verify_user', to: 'users#authentication_user', as: :verify_user
